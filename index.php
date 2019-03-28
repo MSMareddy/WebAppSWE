b77acd15afe29eedc5957a30e0392f06cbdd2361
Venkata Gururamiah <gvenkat96@gmail.com>
2019-03-28 16:39:43 -0500

    Added icon and title
    pizza icon
    Title is Restaurant Picker
    Fixed indentation

--- index.php
+++ index.php
@@ -1,90 +1,88 @@
 <!--testing website map-app-swe.herokuapp.com -->
-<head>
-<title>UI</title>
- <script>
-      // This example requires the Places library. Include the libraries=places
-      // parameter when you first load the API. For example:
-      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title>Restaurant Picker</title>
+		<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+		<meta charset="utf-8">
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+		<script>
+			var map;
+			var service;
+			var infowindow;
 
-      var map;
-      var service;
-      var infowindow;
+			function initMap() {
+			var sydney = new google.maps.LatLng(-33.867, 151.195);
 
-      function initMap() {
-        var sydney = new google.maps.LatLng(-33.867, 151.195);
+			infowindow = new google.maps.InfoWindow();
 
-        infowindow = new google.maps.InfoWindow();
+			map = new google.maps.Map(
+				document.getElementById('map'), {center: sydney, zoom: 15});
 
-        map = new google.maps.Map(
-            document.getElementById('map'), {center: sydney, zoom: 15});
+			var request = {
+			  query: 'Museum of Contemporary Art Australia',
+			  fields: ['name', 'geometry'],
+			};
 
-        var request = {
-          query: 'Museum of Contemporary Art Australia',
-          fields: ['name', 'geometry'],
-        };
+			service = new google.maps.places.PlacesService(map);
 
-        service = new google.maps.places.PlacesService(map);
+			service.findPlaceFromQuery(request, function(results, status) {
+			  if (status === google.maps.places.PlacesServiceStatus.OK) {
+				for (var i = 0; i < results.length; i++) {
+				  createMarker(results[i]);
+				}
 
-        service.findPlaceFromQuery(request, function(results, status) {
-          if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-              createMarker(results[i]);
-            }
+				map.setCenter(results[0].geometry.location);
+			  }
+			});
+			}
 
-            map.setCenter(results[0].geometry.location);
-          }
-        });
-      }
+			function createMarker(place) {
+			var marker = new google.maps.Marker({
+			  map: map,
+			  position: place.geometry.location
+			});
 
-      function createMarker(place) {
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-        });
-
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(place.name);
-          infowindow.open(map, this);
-        });
-      }
-    </script>
-	<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
-</head>
+			google.maps.event.addListener(marker, 'click', function() {
+			  infowindow.setContent(place.name);
+			  infowindow.open(map, this);
+			});
+			}
+		</script>
+	</head>
 <body>
- <?php
+	<?php
 		$option = $_POST["option"] != "" ?$_POST["option"]:"pizza";
 		$price = $_POST["price"] != ""?$_POST["price"]:"0";
 	?>
-<center>
-<table align="left" width ="69%">
-  <tr>
-    <td width="200px" height="200px">
-	<form method = "post">
-		Select restaurant option:
-		<select name ="option">
-			<option value="taco"<?php if($option == "taco"){?> selected <?php }?>>Taco</option>
-			<option value="pizza" <?php if($option == "pizza"){?> selected <?php }?>>Pizza</option>
-			<option value="hamburger" <?php if($option == "hamburger"){?> selected <?php }?>>Hamburger</option>
-		</select>
-		<br><br>
-		Select price range:
-		<div class="range">
-			<input type="range" name ="price" min="0" max="4" steps="1" value="<?php echo $price; ?>">
-		</div>
-		Cheap&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Expensive<br><br><br>
-		<input type = "submit">
-	</form>
-	</td>
-	<td rowspan = "2" width="200px" height="200px">
-		<div id="map" style="width:100%; height:100%"></div>
-		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCDm9MtndFo7OyIi8HvTx4NnUDq9xN5BKE&libraries=places&callback=initMap" async defer></script>
-	</td>
-  </tr>
-  <!-- getting all the Post arguments -->
-  Arguments:
-  <?php echo $option; ?>
-  <?php echo $price; ?>
-</table>
-</center>
+	<table align="left" width ="69%">
+		<tr>
+			<td width="200px" height="200px">
+				<form method = "post">
+					Select restaurant option:
+					<select name ="option">
+						<option value="taco"<?php if($option == "taco"){?> selected <?php }?>>Taco</option>
+						<option value="pizza" <?php if($option == "pizza"){?> selected <?php }?>>Pizza</option>
+						<option value="hamburger" <?php if($option == "hamburger"){?> selected <?php }?>>Hamburger</option>
+					</select>
+					<br><br>
+					Select price range:
+					<div class="range">
+						<input type="range" name ="price" min="0" max="4" steps="1" value="<?php echo $price; ?>">
+					</div>
+					Cheap&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Expensive<br><br><br>
+					<input type = "submit">
+				</form>
+			</td>
+			<td rowspan = "2" width="200px" height="200px">
+				<div id="map" style="width:100%; height:100%"></div>
+				<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCDm9MtndFo7OyIi8HvTx4NnUDq9xN5BKE&libraries=places&callback=initMap" async defer></script>
+			</td>
+		</tr>
+		<!-- getting all the Post arguments -->
+		Arguments:
+		<?php echo $option; ?>
+		<?php echo $price; ?>
+	</table>
 </body>
+</html>
