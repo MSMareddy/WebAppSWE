578b390c64635d25ef41f440e0fdc3cd35e7e1b8
Venkata Gururamiah <gvenkat96@gmail.com>
2019-04-04 15:51:07 -0500

    Syntax error on Added more marker info on marker click event listener
    Removed +

--- index.php
+++ index.php
@@ -99,7 +99,7 @@ try
 			});
 
 			google.maps.event.addListener(marker, 'click', function() {
-			  infowindow.setContent(place.name + "<br>" + place.formatted_address+ "<br>" +
+			  infowindow.setContent(place.name + "<br>" + place.formatted_address+ "<br>" 
 			  + "<a target= ""_blank"" href = ""https://www.google.com/maps/search/?api=1&query=+" + place.name +"+&query_place_id=" + place.place_id+""">View on Google Maps</a>");
 			  infowindow.open(map, this);
 			});
