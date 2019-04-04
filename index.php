bc281d41c55b505ec4d76599932afca19cee130c
Mason Chamberlain <mchamberlain@unomaha.edu>
2019-04-04 12:18:13 -0500

    Updated Zoom
    Zoom set to 7

--- index.php
+++ index.php
@@ -70,7 +70,7 @@ try
 			infowindow = new google.maps.InfoWindow();
 
 			map = new google.maps.Map(
-				document.getElementById('map'), {center: PKI, zoom: 5});
+				document.getElementById('map'), {center: PKI, zoom: 7});
 
 			var request = {
 			  query: '<?php echo $option; ?>',
