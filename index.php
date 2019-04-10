3906fe737c0b963bdc72d7d2551c2fbda4984352
emontemayor <emiliano.montemayor@gmail.com>
2019-04-10 17:25:37 -0500

    Update index.php

--- index.php
+++ index.php
@@ -56,11 +56,18 @@ try
 		<!-- Icon from https://gauger.io/fonticon/ -->
 		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
 		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+		
+		<!-- Smart HTML resources-->
 		<link rel="stylesheet" href="../../source/styles/smart.default.css" type="text/css" />
+		<link rel="stylesheet" href="../../source/styles/smart.base.css" type="text/css" />
 		<link rel="stylesheet" href="../../source/styles/images" type="text/css" />
+		
 		<script type="text/javascript" src="../../source/smart.core.js"></script>
 		<script type="text/javascript" src="../../source/smart.element.js"></script>
 		<script type="text/javascript" src="../../source/smart.button.js"></script>
+		<script type="text/javascript" src="../../source/smart.scrollbar.js"></script>
+		<script type="text/javascript" src="../../source/smart.listbox.js"></script>
+		<script type="text/javascript" src="../../source/smart.dropdownlist.js"></script>
 		<!-- <?php echo "Radius: ", $radius, " Address: ", $address; ?> -->
 		
 		<script>
