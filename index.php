ca14f0d8cde9d22f81d8c176792a4d394b9d022a
emontemayor <emiliano.montemayor@gmail.com>
2019-04-14 16:20:17 -0500

    Testing drop down menu update

--- index.php
+++ index.php
@@ -55,7 +55,8 @@ try
 		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
 		<link rel="stylesheet" type="text/css" href="stylesheet.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
-		  
+		<script src="Elements.js"></script>
+		
 		<!-- <?php echo "Radius: ", $radius, " Address: ", $address; ?> -->
 		
 		<script>
@@ -238,21 +239,25 @@ try
 			Select restaurant option:
 		</div>
 		<div class="TypeListBox">
-			<select name ="option">
-				<option value="taco" <?php if($option == "taco"){?> selected <?php }?>>Taco</option>
-				<option value="pizza" <?php if($option == "pizza"){?> selected <?php }?>>Pizza</option>
-				<option value="hamburger" <?php if($option == "hamburger"){?> selected <?php }?>>Hamburger</option>
-				<option value="pasta" <?php if($option == "pasta"){?> selected <?php }?>>Pasta</option>
-				<option value="steak" <?php if($option == "steak"){?> selected <?php }?>>Steak</option>
-				<option value="seafood" <?php if($option == "seafood"){?> selected <?php }?>>Seafood</option>
-				<option value="sushi" <?php if($option == "sushi"){?> selected <?php }?>>Sushi</option>
-				<option value="chinese food" <?php if($option == "chinese food"){?> selected <?php }?>>Chinese</option>
-				<option value="cupcakes" <?php if($option == "cupcakes"){?> selected <?php }?>>Cupcakes</option>
-				<option value="ice cream" <?php if($option == "ice cream"){?> selected <?php }?>>Ice Cream</option>
-				<option value="chicken" <?php if($option == "chicken"){?> selected <?php }?>>Chicken</option>
-				<option value="sandwich" <?php if($option == "sandwich"){?> selected <?php }?>>Sandwich</option>
-				<option value="buffet" <?php if($option == "buffet"){?> selected <?php }?>>Buffet</option>
-			</select>
+			<div id="dd" class="wrapper-dropdown-3" tabindex"1">
+				<span>Food</span>
+				<ul class="dropdown">
+					<select name ="option">
+						<option value="taco" <?php if($option == "taco"){?> selected <?php }?>>Taco</option>
+						<option value="pizza" <?php if($option == "pizza"){?> selected <?php }?>>Pizza</option>
+						<option value="hamburger" <?php if($option == "hamburger"){?> selected <?php }?>>Hamburger</option>
+						<option value="pasta" <?php if($option == "pasta"){?> selected <?php }?>>Pasta</option>
+						<option value="steak" <?php if($option == "steak"){?> selected <?php }?>>Steak</option>
+						<option value="seafood" <?php if($option == "seafood"){?> selected <?php }?>>Seafood</option>
+						<option value="sushi" <?php if($option == "sushi"){?> selected <?php }?>>Sushi</option>
+						<option value="chinese food" <?php if($option == "chinese food"){?> selected <?php }?>>Chinese</option>
+						<option value="cupcakes" <?php if($option == "cupcakes"){?> selected <?php }?>>Cupcakes</option>
+						<option value="ice cream" <?php if($option == "ice cream"){?> selected <?php }?>>Ice Cream</option>
+						<option value="chicken" <?php if($option == "chicken"){?> selected <?php }?>>Chicken</option>
+						<option value="sandwich" <?php if($option == "sandwich"){?> selected <?php }?>>Sandwich</option>
+						<option value="buffet" <?php if($option == "buffet"){?> selected <?php }?>>Buffet</option>
+					</select>
+				</ul>
 		</div>
 		<div  class="OptionLabel">
 			Select price range:
