2fe3680e73ea5ecf4f2924f5c1e9211fda725e2d
emontemayor <emiliano.montemayor@gmail.com>
2019-04-15 21:52:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -57,7 +57,8 @@ try
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 		<script type="text/javascript" src="Elements.js"></script>
-		
+		 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		 
 		<!-- <?php echo "Radius: ", $radius, " Address: ", $address; ?> -->
 		
 		<script>
