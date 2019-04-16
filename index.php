7f8db1d7b7c9a45bd76a04b47331156624420f30
emontemayor <emiliano.montemayor@gmail.com>
2019-04-15 21:10:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -55,6 +55,7 @@ try
 		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
 		<link rel="stylesheet" type="text/css" href="stylesheet.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 		<script type="text/javascript" src="Element.js"></script>
 		
 		<!-- <?php echo "Radius: ", $radius, " Address: ", $address; ?> -->
