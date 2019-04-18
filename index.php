b8dd46ccb1c4517f99e6e98790f486f16bdddbba
Miti Mareddy <miti.s.mareddy@gmail.com>
2019-04-18 12:40:48 -0500

    updated background

--- index.php
+++ index.php
@@ -350,7 +350,7 @@ try
 		}
 		</script>
 	</head>
-<body background="banans.jpg">
+<body>
 	<ul>
 	  <li><a class="active" href="/">Home</a></li>
 	  <li><a id="getLocation" href="/">Change Location</a></li>
