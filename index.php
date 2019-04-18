ee16763404e94f4a14e9d7dc40c7a88f953fb9c4
Miti Mareddy <miti.s.mareddy@gmail.com>
2019-04-18 12:27:50 -0500

    fruits

--- index.php
+++ index.php
@@ -350,8 +350,7 @@ try
 		}
 		</script>
 	</head>
-<body>
-<background = "fruits.png">
+<body background="fruits.png">
 	<ul>
 	  <li><a class="active" href="/">Home</a></li>
 	  <li><a id="getLocation" href="/">Change Location</a></li>
